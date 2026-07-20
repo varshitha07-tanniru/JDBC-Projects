b55046f9681031050940de5100ce404f46bc05c9
Tanniru Varshitha <tanniruvarshitha@gmail.com>
2026-07-20 20:59:57 +0530

    Intial commit

---
+++ src/EcommerceProject/SqlConnection.java
@@ -0,0 +1,33 @@
+package EcommerceProject;
+import java.sql.*;
+public class SqlConnection {
+	public static void main(String[] args) {
+		//load and register the jdbc
+		Connection con=null;
+		try {
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		//Connection 
+		String url="jdbc:mysql://localhost:3306/ecommerce_db";
+		String user="root";
+		String password="Varshitha@09";
+		con=DriverManager.getConnection(url,user,password);
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+		finally {
+			try {
+				if(con!=null)
+				con.close();
+			}
+			catch(Exception e)
+			{
+				e.printStackTrace();
+			}
+		}
+		System.out.println("Hello");
+		
+	}
+}
+
